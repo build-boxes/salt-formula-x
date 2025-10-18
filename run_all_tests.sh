d608bf2d63c29895482c23e947bf7ec757e386ed
Hammad Rauf <rauf.hammad@gmail.com>
2025-10-18 16:27:36 -0400

    Added script for running all serverspec tests.

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-shopt -s globstar
+# shopt -s globstar
+
+# echo "===> Running all Serverspec tests..."
+# if rspec /srv/salt/formula/**/test/*_spec.rb; then
+#   echo "===> All tests passed."
+# else
+#   echo "===> Some tests failed."
+#   exit 1
+# fi
 
 echo "===> Running all Serverspec tests..."
-if rspec /srv/salt/formula/**/test/*_spec.rb; then
-  echo "===> All tests passed."
-else
-  echo "===> Some tests failed."
-  exit 1
-fi
 
+# Find all *_spec.rb files under formula/**/test and run them individually
+find /srv/salt/formula -type f -path '*/test/*_spec.rb' | while read -r specfile; do
+  echo "Running: $specfile"
+  rspec "$specfile"
+done
+
+echo "===> All tests completed."
