cc718ef6756fa4b3f0475e028f60aed94b0ac628
Hammad Rauf <rauf.hammad@gmail.com>
2025-10-19 15:32:30 -0400

    Fixed examples and Shell comments, for later reference.

--- bootstrap_minion.sh
+++ bootstrap_minion.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 echo "===> Bootstrapping Salt master/minion..."
-systemctl start salt-master
-systemctl start salt-minion
-sleep 5
+echo "===> Restarting Salt master/minion services..."
+systemctl restart salt-master
+systemctl restart salt-minion
+echo "===> Waiting for 8 seconds..."
+sleep 8
+echo "===> Testing Salt minion connectivity to master..."
 salt-call test.ping
-salt-call state.apply
+echo "===> Testing Salt master connectivity to minion..."
+salt "local-master" test.ping
+echo "===> Check salt Master All Keys..."
+salt-key -L
+echo "===> Now You can apply Salt states..."
+echo "For example:"
+echo " - From the minion:"
+echo "    - salt-call state.apply"
+echo "    - salt-call state.apply <state_name>"
+echo " - From the master:"
+echo "    - salt 'local-master' state.apply"
+echo "    - salt 'local-master' state.apply <state_name>"
 echo "===> Salt master/minion bootstrap complete."
-
--- run_all_tests.sh
+++ run_all_tests.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# shopt -s globstar
-
-# echo "===> Running all Serverspec tests..."
-# if rspec /srv/salt/formula/**/test/*_spec.rb; then
-#   echo "===> All tests passed."
-# else
-#   echo "===> Some tests failed."
-#   exit 1
-# fi
-
 echo "===> Running all Serverspec tests..."
 
+echo "===> Find all *_spec.rb files under formula/**/test/*_spec.rb and run them individually."
+echo "     The first line of all tests should be: require '/opt/serverspec/spec_helper'"
 # Find all *_spec.rb files under formula/**/test and run them individually
 find /srv/salt/formula -type f -path '*/test/*_spec.rb' | while read -r specfile; do
   echo "Running: $specfile"
